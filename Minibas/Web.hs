8a1aa9861ea374135ff4e484d28e4e20e445b11c
jabaraster <jabaraster@gmail.com>
2016-06-26 23:53:48 +0900

    mainte.

--- Minibas/Web.hs
+++ Minibas/Web.hs
@@ -2,7 +2,7 @@
 
 module Minibas.Web where
 
-import Model (Game)
+import Model (Game, Score)
 
 import ClassyPrelude.Yesod
 import Jabara.Yesod.Util (tc, ttc)
@@ -12,3 +12,6 @@ instance ToTypedContent (Entity Game) where toTypedContent = ttc
 
 instance ToContent [Entity Game] where toContent = tc
 instance ToTypedContent [Entity Game] where toTypedContent = ttc
+
+instance ToContent (Entity Score) where toContent = tc
+instance ToTypedContent (Entity Score) where toTypedContent = ttc
